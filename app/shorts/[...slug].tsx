cb4227da66c5df6813860baeae362969801bf329
dwhddnjs <per1215@naver.com>
2025-05-17 03:40:55 +0900

    📦 expo 캐러셀 추가

--- app/shorts/[...slug].tsx
+++ app/shorts/[...slug].tsx
@@ -1,30 +1,40 @@
 import { Text, View } from "@/components/Themed"
+import { useShortsStore } from "@/hooks/use-shorts-store"
 import { useEvent } from "expo"
 import { useLocalSearchParams, useNavigation } from "expo-router"
 import { useVideoPlayer, VideoView } from "expo-video"
 import React from "react"
 import { StyleSheet } from "react-native"
+import PagerView from "react-native-pager-view"
 
 export default function ShortsView() {
   const { slug } = useLocalSearchParams<any>()
+  const { videos } = useShortsStore()
   const uri = slug.join("/")
-  const player = useVideoPlayer(uri, (player) => {
-    player.loop = true
-    player.play()
-  })
-  const { isPlaying } = useEvent(player, "playingChange", {
-    isPlaying: player.playing,
-  })
 
   return (
-    // <View style={{ flex: 1 }}>
-    <VideoView
-      style={styles.video}
-      player={player}
-      allowsFullscreen
-      allowsPictureInPicture
-    />
-    // </View>
+    <View style={{ flex: 1 }}>
+      <PagerView style={{ flex: 1 }} initialPage={0} orientation={"vertical"}>
+        {videos.map((item) => {
+          const player = useVideoPlayer(item.video, (player) => {
+            player.loop = true
+            player.play()
+          })
+          const { isPlaying } = useEvent(player, "playingChange", {
+            isPlaying: player.playing,
+          })
+          return (
+            <VideoView
+              key={item.id}
+              style={styles.video}
+              player={player}
+              allowsFullscreen
+              allowsPictureInPicture
+            />
+          )
+        })}
+      </PagerView>
+    </View>
   )
 }
 
@@ -33,5 +43,9 @@ const styles = StyleSheet.create({
     flex: 1,
     // width: 350,
     // height: 275,
+  },
+  page: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 })
